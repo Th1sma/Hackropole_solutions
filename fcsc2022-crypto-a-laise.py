f1df94213fb0f2dc3f153d6bce502a9b8e81dda7
Th1sma <mathis.niveau17@outlook.com>
2024-04-08 14:06:32 +0200

    Ajout d'une épreuve

---
+++ fcsc2022-crypto-a-laise.py
@@ -0,0 +1,21 @@
+def vigenere_decrypt(ciphertext, key):
+    plaintext = ""
+    key_length = len(key)
+    key_index = 0
+    for char in ciphertext:
+        if char.isalpha():
+            shift = ord(key[key_index].upper()) - ord('A')
+            if char.islower():
+                plaintext += chr(((ord(char) - ord('a') - shift) % 26) + ord('a'))
+            else:
+                plaintext += chr(((ord(char) - ord('A') - shift) % 26) + ord('A'))
+            key_index = (key_index + 1) % key_length
+        else:
+            plaintext += char
+    return plaintext
+
+ciphertext = "Gqfltwj emgj clgfv ! Aqltj rjqhjsksg ekxuaqs, ua xtwk n'feuguvwb gkwp xwj, ujts f'npxkqvjgw nw tjuwcz ugwygjtfkf qz uw efezg sqk gspwonu. Jgsfwb-aqmu f Pspygk nj 29 cntnn hqzt dg igtwy fw xtvjg rkkunqf."
+key = "FCSC"
+
+decrypted_message = vigenere_decrypt(ciphertext, key)
+print(decrypted_message)
