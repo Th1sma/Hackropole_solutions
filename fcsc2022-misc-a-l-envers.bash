ca4b7948ad3d51332a4addaf1f1ba6bfdc365ff5
Th1sma <mathis.niveau17@outlook.com>
2024-04-10 15:06:19 +0200

    Update : new challenge

---
+++ fcsc2022-misc-a-l-envers.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Connexion à localhost:4000..."
+
+exec 3<>/dev/tcp/localhost/4000
+
+while read line <&3; do
+    echo $line
+    if [[ $line == \>\>\>* ]]; then
+        nline=$(echo $line |rev |tr -d '>>>')
+        echo $nline
+        echo $nline >&3
+    fi
+done
+
+echo "Fermeture de la connexion..."
+exec 3<&-
