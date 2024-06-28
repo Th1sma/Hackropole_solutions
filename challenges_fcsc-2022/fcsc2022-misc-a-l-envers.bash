9bd4b6fd95a3b0adeaa23e70746c7e28739f3c8c
Th1sma <mathis.niveau17@outlook.com>
2024-06-28 13:48:57 +0200

    :recycle: test pre-commit

--- challenges_fcsc-2022/fcsc2022-misc-a-l-envers.bash
+++ challenges_fcsc-2022/fcsc2022-misc-a-l-envers.bash
@@ -4,14 +4,16 @@ echo "Connexion à localhost:4000..."
 
 exec 3<>/dev/tcp/localhost/4000
 
-while read line <&3; do
-    echo $line
+while read -r line <&3; do
+    echo "$line"
     if [[ $line == \>\>\>* ]]; then
-        nline=$(echo $line |rev |tr -d '>>>')
-        echo $nline
-        echo $nline >&3
+        nline=$(echo "$line" | rev | tr -d '>>>')
+        echo "$nline"
+        echo "$nline" >&3
     fi
 done
 
 echo "Fermeture de la connexion..."
 exec 3<&-
+
+#test
